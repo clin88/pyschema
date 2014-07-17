c4f12e2961d9ef9105698978892ed61d3b6c2471
Chen Lin <clin_@berkeley.edu>
2014-07-16 17:45:34 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,69 @@
+# Pyschema
+
+Simple, composable, and extensible schema validation and coercion tool. Based on 
+[schema](https://github.com/halst/schema) but with more extensible code, added syntax
+features and active development.
+
+## Installation
+
+Not on pypi yet, so you'll have to clone the repo. Sorry!
+
+## Use
+
+### Basic Validation
+
+Create a `Schema` object by passing a map, type, callable, or list to `Schema`. Then
+call `validate` on that object to get a True or False validation.
+
+```.py
+
+s = Schema({
+    'name': str,
+    'age': int,
+})
+assert s.validate({'name': 'Chen', 'age': 26})
+
+s = Schema([int])
+assert s.validate([1])
+
+s = Schema(str)
+assert s.validate('abc')
+```
+
+### Schema descriptors
+
+There's also support for custom Schema descriptors for optional values, regexes,
+and custom validation functions.
+
+```.py
+def cardchecksum(n):
+    # validates credit card number, returns True or False
+    ...
+
+s = Schema({
+    'creditcard': Check(cardchecksum),
+    'zipcode': Optional(int, default=10101)
+})
+assert s.validate({ 'creditcard': 12345678911111111 })
+```
+
+### Logical Combinators
+
+You can mix and match Schemas with `And` and `Or`. The results of the logical
+operators `&` and `|` are syntatical sugar that produce `And` and `Or`, respectively.
+
+### Coercion
+
+Some schema descriptors can return coerced versions of your values. 
+
+You can also create a copy of your data with the values coerced by callables using
+the `Coerce` descriptor with the `coerce` function.  
+
+```python
+s = Schema({
+    'age': Coerce(int)
+    })
+print s.coerce({'age': '26'})
+
+>>> { 'age': 26 }
+```
---
+++ schema.py
@@ -0,0 +1,192 @@
+"""Simple framework for validating data against a schema.
+
+Schemas are simply dictionaries with expected types in place of
+values. All keys in schema are required, and all values of required
+keys must be an instance of the required type. Note that instances
+of subclasses of the required type are valid.
+"""
+# TODO: Load Schema from schema descriptor formats?
+# TODO: Boolean operators for schemas
+# TODO: __instancecheck__ hook
+import re
+import abc
+import collections
+
+class Empty(object):
+    pass
+
+EMPTY = Empty()
+
+def dispatch(schema):
+    if isinstance(schema, SchemaDescriptor):
+        return schema
+    elif isinstance(schema, list):
+        return List(schema)
+    elif isinstance(schema, dict):
+        return Map(schema)
+    #elif isinstance(schema, set):
+    #    return Set(schema)
+    elif isinstance(schema, type):
+        return Instance(schema)
+    else:
+        raise TypeError("{} not a valid schema descriptor.".format(schema))
+
+class SchemaDescriptor(object):
+    __metaclass__ = abc.ABCMeta
+    typ = None
+
+    def __init__(self, schema):
+        if not isinstance(schema, self.typ):
+            raise TypeError("Schema {} must be type {}.".format(schema, self.typ))
+
+        self.schema = schema
+
+    @abc.abstractmethod
+    def validate(self, data):
+        """Validates the data according to the descriptor's rules.
+
+        If valid, return the data (plus any transformations necessary).
+        If not valid, throw TypeError.
+        """
+        pass
+
+    # TODO: Coerce vs validate distinction
+    def coerce(self, data):
+        pass
+
+class RegEx(SchemaDescriptor):
+    def __init__(self, pattern, flags=0):
+        self.pattern = pattern
+        self.flags = flags
+
+    def validate(self, data):
+        try:
+            result = re.search(self.pattern, data, flags=self.flags)
+        except (TypeError, ValueError):
+            raise ValueError
+
+        if result:
+            return data
+        else:
+            raise ValueError
+
+class Email(RegEx):
+    def __init__(self):
+        self.pattern = '^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]+$'
+        self.flags = 0
+
+##############
+# BASE CASES #
+##############
+
+class Instance(SchemaDescriptor):
+    typ = type
+    def validate(self, data):
+        if isinstance(data, self.schema):
+            return data
+        else:
+            raise ValueError
+
+class Coerce(SchemaDescriptor):
+    def __init__(self, schema):
+        if not callable(schema):
+            raise TypeError('Arguments to Coerce must be callable.')
+        self.schema = schema
+
+    def validate(self, data):
+        return self.schema(data)
+
+class Check(SchemaDescriptor):
+    def __init__(self, schema):
+        if not callable(schema):
+            raise TypeError('Arguments to Check must be callable.')
+        self.schema = schema
+
+    def validate(self, data):
+        if self.schema(data):
+            return data
+
+        raise ValueError
+
+###############
+# COLLECTIONS #
+###############
+
+class List(SchemaDescriptor):
+    typ = list
+    def __init__(self, schema):
+        if not isinstance(schema, list):
+            raise TypeError('List only takes lists as schema. {} is not a list.'.format(schema))
+
+        if len(schema) != 1:
+            raise TypeError('Only homogenous containers allowed.')
+
+        self.schema = dispatch(schema[0])
+
+    def validate(self, data):
+        if not isinstance(data, list):
+            raise ValueError
+
+        # TODO: schema_type property?
+        new = []
+        for item in data:
+            new.append(self.schema.validate(item))
+
+        return new
+
+class Map(SchemaDescriptor):
+    def __init__(self, schema):
+        if not isinstance(schema, collections.MutableMapping):
+            raise TypeError("Map may only take mappings as schema. {} is not a mapping.".format(schema))
+
+        self.schema = schema
+
+    def validate(self, data):
+        if not isinstance(data, collections.MutableMapping):
+            raise ValueError
+
+        new = {}
+        keys = set(data.keys())
+        for key, schema in self.schema.iteritems():
+            val = data.get(key, EMPTY)
+            schema = dispatch(schema)
+            new[key] = schema.validate(val)
+            keys.remove(key)
+
+        if keys:
+            raise ValueError
+
+        return new
+
+#############
+# OPTIONALS #
+#############
+
+class Optional(SchemaDescriptor):
+    def __init__(self, schema, default=None):
+        if default and not isinstance(default, schema):
+            raise ValueError("Default {} must validate against type {}.".format(default, schema))
+
+        self.schema = dispatch(schema)
+        self.default = default
+
+    def validate(self, data):
+        if data is EMPTY:
+            return self.default
+        else:
+            return self.schema.validate(data)
+
+class OptionalString(Optional):
+    def __init__(self, default):
+        if default and not isinstance(default, basestring):
+            raise ValueError("OptionalString requires a default string. {} is not a string.".format(default))
+
+        self.schema = dispatch(str)
+        self.default = default
+
+    def validate(self, data):
+        if data == '' or data is EMPTY:
+            return self.default
+        else:
+            return self.schema.validate(data)
+
